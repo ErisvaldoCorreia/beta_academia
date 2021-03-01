1a6f5fbd64ed8f6761fe1189704d4eabf31d489d
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2021-03-01 08:51:31 -0300

    constroi paginator e define positions

--- lib/onboarding.page.dart
+++ lib/onboarding.page.dart
@@ -8,27 +8,71 @@ class Onboarding extends StatefulWidget {
 }
 
 class _OnboardingState extends State<Onboarding> {
+  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[100],
-      body: PageView.builder(
-        itemCount: content.length,
-        itemBuilder: (_, i) {
-          return Padding(
-            padding: const EdgeInsets.all(40.0),
-            child: Column(
-              children: [
-                SvgPicture.asset(content[i].image, height: 350,),
-                Text(content[i].title, style: GoogleFonts.nunito(fontSize: 24, fontWeight: FontWeight.bold, color: Color(0xffec3337)),), //#fcee22
-                SizedBox(height: 10,),
-                Text(content[i].description, textAlign: TextAlign.center, style: GoogleFonts.nunito(fontSize: 14, color: Colors.grey),)
-              ],
+      body: Column(
+        children: [
+          Container(
+            height: 150,
+            width: double.infinity,
+            color: Colors.green,
+          ),
+          Expanded(
+            child: PageView.builder(
+              itemCount: content.length,
+              onPageChanged: (int index) {
+                setState(() {
+                  currentIndex = index;
+                });
+              },
+              itemBuilder: (_, i) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 30.0),
+                  child: Column(
+                    children: [
+                      SvgPicture.asset(content[i].image, height: 300,),
+                      Text(content[i].title, style: GoogleFonts.nunito(fontSize: 24, fontWeight: FontWeight.bold, color: Color(0xffec3337)),), //#fcee22
+                      SizedBox(height: 10,),
+                      Text(content[i].description, textAlign: TextAlign.center, style: GoogleFonts.nunito(fontSize: 14, color: Colors.grey),)
+                    ],
+                  ),
+                );
+              }
             ),
-          );
-        }
+          ),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: 
+                List.generate(
+                  content.length, 
+                  (index) => buildDots(index),
+                ),
+            ),
+          ),
+           Container(
+            height: 200,
+            width: double.infinity,
+            color: Colors.green,
+          ),
+        ],
       ),
     );
+  }
+
+  Container buildDots(int index) {
+    return Container(
+                  margin: EdgeInsets.only(right: 10.0),
+                  height: 10,
+                  width: currentIndex == index ? 25 : 10,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.red,
+                  ),
+                );
   }
 }
 
