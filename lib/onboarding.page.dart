440e50b2a9f114451598705b3750b98bf9edd79c
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2021-03-01 08:28:19 -0300

    criando a classe base para boarding

--- lib/onboarding.page.dart
+++ lib/onboarding.page.dart
@@ -13,16 +13,16 @@ class _OnboardingState extends State<Onboarding> {
     return Scaffold(
       backgroundColor: Colors.grey[100],
       body: PageView.builder(
-        itemCount: 3,
+        itemCount: content.length,
         itemBuilder: (_, i) {
           return Padding(
             padding: const EdgeInsets.all(40.0),
             child: Column(
               children: [
-                SvgPicture.asset('assets/image3.svg', height: 350,),
-                Text('Treinamento Diário', style: GoogleFonts.nunito(fontSize: 24, fontWeight: FontWeight.bold, color: Color(0xffec3337)),), //#fcee22
+                SvgPicture.asset(content[i].image, height: 350,),
+                Text(content[i].title, style: GoogleFonts.nunito(fontSize: 24, fontWeight: FontWeight.bold, color: Color(0xffec3337)),), //#fcee22
                 SizedBox(height: 10,),
-                Text('A prática constante dos exercícios promove o equilíbrio fisico e mental, além de melhorar a nossa saúde!', textAlign: TextAlign.center, style: GoogleFonts.nunito(fontSize: 14, color: Colors.grey),)
+                Text(content[i].description, textAlign: TextAlign.center, style: GoogleFonts.nunito(fontSize: 14, color: Colors.grey),)
               ],
             ),
           );
@@ -31,3 +31,17 @@ class _OnboardingState extends State<Onboarding> {
     );
   }
 }
+
+class OnboardingContent {
+  String image;
+  String title;
+  String description;
+
+  OnboardingContent({this.image, this.title, this.description});
+}
+
+List<OnboardingContent> content = [
+  OnboardingContent(title: "Treinamento Diário", image: "assets/image1.svg", description: "A prática constante dos exercícios promove o equilíbrio fisico e mental, além de melhorar a nossa saúde!",),
+  OnboardingContent(title: "Profissionais Treinados", image: "assets/image3.svg", description: "Aqui virá uma frase de efeito a ser definida ainda",),
+  OnboardingContent(title: "Acolhimento", image: "assets/image2.svg", description: "Aqui virá uma frase de efeito a ser definida ainda",),
+];
