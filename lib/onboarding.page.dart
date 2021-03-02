5a1375776cf70a734f8d5f7f0301c61c6e298318
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2021-03-02 08:35:23 -0300

    criando o paginator manual

--- lib/onboarding.page.dart
+++ lib/onboarding.page.dart
@@ -18,61 +18,81 @@ class _OnboardingState extends State<Onboarding> {
           Container(
             height: 150,
             width: double.infinity,
-            color: Colors.green,
+            //color: Colors.green,
           ),
           Expanded(
-            child: PageView.builder(
-              itemCount: content.length,
-              onPageChanged: (int index) {
-                setState(() {
-                  currentIndex = index;
-                });
-              },
-              itemBuilder: (_, i) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 30.0),
-                  child: Column(
-                    children: [
-                      SvgPicture.asset(content[i].image, height: 300,),
-                      Text(content[i].title, style: GoogleFonts.nunito(fontSize: 24, fontWeight: FontWeight.bold, color: Color(0xffec3337)),), //#fcee22
-                      SizedBox(height: 10,),
-                      Text(content[i].description, textAlign: TextAlign.center, style: GoogleFonts.nunito(fontSize: 14, color: Colors.grey),)
-                    ],
-                  ),
-                );
-              }
-            ),
+            child: buildPageBoard(),
           ),
           Container(
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: 
-                List.generate(
-                  content.length, 
-                  (index) => buildDots(index),
-                ),
+              children: List.generate(
+                content.length,
+                (index) => buildDots(index),
+              ),
             ),
           ),
-           Container(
-            height: 200,
+          Container(
+            height: 150,
             width: double.infinity,
-            color: Colors.green,
+            //color: Colors.green,
           ),
         ],
       ),
     );
   }
 
-  Container buildDots(int index) {
-    return Container(
-                  margin: EdgeInsets.only(right: 10.0),
+  PageView buildPageBoard() {
+    return PageView.builder(
+        itemCount: content.length,
+        onPageChanged: (int index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        itemBuilder: (_, i) {
+          return Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 40.0, vertical: 30.0),
+            child: Column(
+              children: [
+                SvgPicture.asset(
+                  content[i].image,
+                  height: 280,
+                ),
+                Text(
+                  content[i].title,
+                  style: GoogleFonts.nunito(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xffec3337)),
+                ), //#fcee22
+                SizedBox(
                   height: 10,
-                  width: currentIndex == index ? 25 : 10,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.red,
-                  ),
-                );
+                ),
+                Text(
+                  content[i].description,
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.nunito(fontSize: 14, color: Colors.grey),
+                )
+              ],
+            ),
+          );
+        });
+  }
+
+  AnimatedContainer buildDots(int index) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 100),
+      curve: Curves.bounceIn,
+      margin: EdgeInsets.only(right: 10.0),
+      height: 8,
+      width: currentIndex == index ? 25 : 8,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: currentIndex == index ? Colors.red : Colors.grey,
+      ),
+    );
   }
 }
 
@@ -85,7 +105,22 @@ class OnboardingContent {
 }
 
 List<OnboardingContent> content = [
-  OnboardingContent(title: "Treinamento Diário", image: "assets/image1.svg", description: "A prática constante dos exercícios promove o equilíbrio fisico e mental, além de melhorar a nossa saúde!",),
-  OnboardingContent(title: "Profissionais Treinados", image: "assets/image3.svg", description: "Aqui virá uma frase de efeito a ser definida ainda",),
-  OnboardingContent(title: "Acolhimento", image: "assets/image2.svg", description: "Aqui virá uma frase de efeito a ser definida ainda",),
+  OnboardingContent(
+    title: "Treinamento Diário",
+    image: "assets/image1.svg",
+    description:
+        "A prática constante dos exercícios promove o equilíbrio fisico e mental, além de melhorar a nossa saúde!",
+  ),
+  OnboardingContent(
+    title: "Acolhimento",
+    image: "assets/image2.svg",
+    description:
+        "Aqui virá uma frase de efeito a ser definida ainda! Aqui virá uma frase de efeito a ser definida ainda",
+  ),
+  OnboardingContent(
+    title: "Profissionais Treinados",
+    image: "assets/image3.svg",
+    description:
+        "Aqui virá uma frase de efeito a ser definida ainda! Aqui virá uma frase de efeito a ser definida ainda",
+  ),
 ];
